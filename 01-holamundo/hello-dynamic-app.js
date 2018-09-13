def739a2ad6322375d891ebfac44795a51a19fe0
Benavente <jbenavem@extern.isban.com>
2018-09-13 17:55:35 +0200

    funccion flecha

--- 01-holamundo/hello-dynamic-app.js
+++ 01-holamundo/hello-dynamic-app.js
@@ -2,11 +2,13 @@ console.log("Introduce tu nomnbre:");
 let stdin = process.openStdin();
 
 //OJO: El campo se debe de llamar data.
-  stdin.addListener("data", function(d) {
+  //stdin.addListener("data", function(d) {
+  stdin.addListener("data", d => {
     // note:  d is an object, and when converted to a string it will
     // end with a linefeed.  so we (rather crudely) account for that  
     // with toString() and then trim() 
-    console.log(`Hola ${d.toString().trim()}`);
+    let name=d.toString().trim();
+    console.log(`Hola ${name}`);
     process.exit();
   });
   
